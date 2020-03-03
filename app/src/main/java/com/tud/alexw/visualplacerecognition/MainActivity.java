0d67627cb8d84c9bb80a4e770c76afb2d2b764d6
lllAlexanderlll <alex.a.weber@gmx.de>
2020-03-03 10:09:10 +0100

    Merged majorityCount into master

--- app/src/main/java/com/tud/alexw/visualplacerecognition/MainActivity.java
+++ app/src/main/java/com/tud/alexw/visualplacerecognition/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mCaptureButton;
     private TextView mTestTextView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
